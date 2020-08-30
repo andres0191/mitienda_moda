7f6f7ba0fa0829a6ed78b7908b9b3996cf365d67
andres0191 <andres_felipe0191@hotmail.com>
2020-08-30 17:53:38 -0500

    update file auth, add responsive when image is < to 700 px, add style when number fail

--- src/components/Authentication/Auth.js
+++ src/components/Authentication/Auth.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './style.scss';
 import ImgMiTienda from '../../img/Login/logomitiendamoda.png';
 import InputMask from 'react-input-mask';
 import 'bootstrap/dist/css/bootstrap.css';
+import {useState} from 'react'
 
-export default function auth() {
+export default function Auth() {
+const [error, setError] = useState(false); 
+const handleOnChange = (event) => {
+    const inputText = event.target.value;
+    inputText !== '786 547 0213' && inputText.length === 12 ? setError(true) : setError(false);
+    console.log(inputText)
+}
 
     return (
-        <div className='back d-flex align-items-center justify-content-center flex-column' style={{ border: '3px solid green' }}>
-            <div style={{ width: '24%', padding: '2rem' }} className='container   mt-md-10 justify-content- d-flex flex-column align-items-center'>
+        <div className='back d-flex align-items-center justify-content-center '>
+            <div className='textBox textBoxResp mt-md-10 justify-content- d-flex flex-column align-items-center'>
                 <br /><br /><br />
-                <div className='row justify-content-start d-flex justify-content-center flex-column logoLogin align-items-center'>
+                <div className='row justify-content-start d-flex justify-content-center logoLogin align-items-center'>
                     <img width='60%' alt="@andres0191" src={ImgMiTienda} className=' pr-1' />
                     <br />
                     <h3 className='textInput'>
@@ -18,10 +25,9 @@ export default function auth() {
                     </h3>
                 </div><br />
                 <div className='input-field'>
-                    <InputMask mask="999 999 9999" maskChar={null} id='name' required className='inputNumber' />
-                    <label for='name'>
-                        Telefono:
-                    </label>
+                    <InputMask mask="999 999 9999" maskChar={null} id='name' required className='inputNumber' onChange={handleOnChange}/>               
+                    {error ? <p style={{color: '#ff5252', fontSize: '12px'}}>Este número no es valido o no está registrado.</p> : ''}
+                    {error ? <label  style={{color: '#ff5252', fontSize: '12px'}}>Telefono</label> :           <label htmlFor='name'> Telefono: </label>}
                 </div>
             </div>
         </div>
