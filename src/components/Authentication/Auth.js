de653d7fa3d85a991f98e57e174a4891c47580b9
andres0191 <andres_felipe0191@hotmail.com>
2020-08-30 16:12:07 -0500

    update auth file

--- src/components/Authentication/Auth.js
+++ src/components/Authentication/Auth.js
@@ -1,33 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './style.scss';
-import ImgMiTienda from '../../img/logomitiendamoda.png';
-
+import ImgMiTienda from '../../img/Login/logomitiendamoda.png';
+import InputMask from 'react-input-mask';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function auth() {
 
-const verifyType =(event)=>{
-    if ( isNaN(event.target.value)){
-        /* event.preventDefault(); */
-        console.log('no number')
-        return;
-    }
-    console.log('number')
-} 
-
-    return(
-        <div className='back  align-center justify-center column fill-height'>
-            <div className='form '>
-                <img width='30%' alt="@andres0191" src={ImgMiTienda} >
-                </img>
-                <h3 className='text1'>
-                Ingresa el numero de telefono<br/>de tu asesora independiente.
-                </h3>
-                {/* <input aria-label="Telefono" type='Number' maxLength='12' /> */}
-                <input
-                className='inputTel'
-                placeholder='Telefono'
-                /* type='number' */
-                onKeyDown={verifyType}/>
+    return (
+        <div className='back d-flex align-items-center justify-content-center flex-column' style={{ border: '3px solid green' }}>
+            <div style={{ width: '24%', padding: '2rem' }} className='container   mt-md-10 justify-content- d-flex flex-column align-items-center'>
+                <br /><br /><br />
+                <div className='row justify-content-start d-flex justify-content-center flex-column logoLogin align-items-center'>
+                    <img width='60%' alt="@andres0191" src={ImgMiTienda} className=' pr-1' />
+                    <br />
+                    <h3 className='textInput'>
+                        Ingresa el numero de telefono<br />de tu asesora independiente.
+                    </h3>
+                </div><br />
+                <div className='input-field'>
+                    <InputMask mask="999 999 9999" maskChar={null} id='name' required className='inputNumber' />
+                    <label for='name'>
+                        Telefono:
+                    </label>
+                </div>
             </div>
         </div>
     )
